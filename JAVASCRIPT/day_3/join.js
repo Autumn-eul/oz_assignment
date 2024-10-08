c65946f62c9e833918e9b2b7b81d7e0a8de716a5
Autumn-eul <dlrkdmf1052@naver.com>
2024-10-08 17:59:00 +0900

    js day_3 end

--- JAVASCRIPT/day_3/join.js
+++ JAVASCRIPT/day_3/join.js
@@ -1,4 +1,3 @@
-// 제출 이벤트를 받는다 (이벤트 핸들링)
 const form = document.getElementById("form")
 
 form.addEventListener("submit", function (event) {
@@ -14,11 +13,37 @@ form.addEventListener("submit", function (event) {
     let userEmail = event.target.email.value
     let userIntro = event.target.intro.value
 
-    console.log(userId, userPw1, userPw2, userName, userPhone,
-        userPosition, userGender, userEmail, userIntro)
+    /* console.log(userId, userPw1, userPw2, userName, userPhone,
+        userPosition, userGender, userEmail, userIntro) */
+
+    if (userId.length < 6) {
+        alert("아이디가 너무 짧습니다. 6자 이상 입력해주세요.")
+        return
+    }
+
+    if (userPw1 !== userPw2) {
+        alert("비밀번호가 일치하지 않습니다.")
+        return
+    }
+
+    if (userIntro.length < 10) {
+        alert("10자 이상 입력하세요.")
+        return
+    }
+
+    document.body.innerHTML = ""
+    document.write(`${userId}님 환영합니다.<br>
+        회원 가입 시 입력하신 내역은 다음과 같습니다.<br>
+        아이디 : ${userId}<br>
+        이름 : ${userName}<br>
+        전화번호 : ${userPhone}<br>
+        원하는 직무 : ${userPosition}`)
+
 })
-// 제출된 입력 값들을 참조한다
 
-// 입력값에 문제가 있는 경우 이를 감지한다
-
-// 가입 환영 인사를 제공한다
+/*
+1. 제출 이벤트를 받는다 (이벤트 핸들링)
+2. 제출된 입력 값들을 참조한다
+3. 입력값에 문제가 있는 경우 이를 감지한다
+4. 가입 환영 인사를 제공한다
+*/
