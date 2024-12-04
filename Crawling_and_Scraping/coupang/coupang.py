53fefda9b2a65d4885b915d7c82283f90e842f09
Autumn-eul <dlrkdmf1052@naver.com>
2024-12-05 00:00:46 +0900

    🌐 웹 스크래핑과 웹 크롤링 7일차
    7일차

--- Crawling_and_Scraping/coupang/coupang.py
+++ Crawling_and_Scraping/coupang/coupang.py
@@ -1,4 +1,7 @@
+import ftplib
+
 from datetime import datetime # 시간 정보를 가져올 때 사용
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
@@ -33,6 +36,8 @@ soup = BeautifulSoup(html, "html.parser") # 분석된 트리구조의 결과를 
 
 items = soup.select("[class=search-product]") # ".search-product"
 
+main_text = ""
+
 for rank, item in enumerate(items, 1): # enumerate? : 반복가능한 객체를 원하는 갯수만큼 넣어서 증가시킬 수 있음
     name = item.select_one(".name").text
     price = item.select_one(".price-value").text
@@ -59,3 +64,86 @@ for rank, item in enumerate(items, 1): # enumerate? : 반복가능한 객체를 
     img_url = img_url.replace("230x230ex", "600x600ex")
     print(f"이미지 URL : {img_url}")
     print("-" * 90)
+    main_text += f"<p><h2>{rank}위 : {name}</h2><b>가격 : {price}</b></p><div><a href='https://www.coupang.com{link}' target='_blank'><div class='img main'><img src='{img_url}' alt='제품 이미지' /></div></div>"
+    if rank == 10:
+        break
+
+now = datetime.now()
+today = f"{now.year}년 {now.month}월 {now.day}일"
+summary = f"{today} 기준 추천 {keyword} Top10"
+
+file_name = 'oz.html'
+title = f"오늘의 추천 {keyword}"
+
+main = f"""<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>{title}</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.html" class="logo">Massively</a>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Post -->
+							<section class="post">
+								<header class="major">
+									<span class="date">{today}</span>
+									<h1>{title}</h1>
+									<p>{summary}</p>
+								</header>
+								{main_text}
+							</section>
+
+					</div>
+
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>"""
+
+with open(f"coupang/{file_name}", "w", encoding="utf-8") as f: # coupang/oz.html
+    f.write(f"{main}")
+    
+ftp_url = "lolll.dothome.co.kr/"
+ftp_id = "lolll"
+ftp_pw = "lll1503120!"
+
+ftp = ftplib.FTP()
+ftp.connect(ftp_url, 21)
+ftp.login(ftp_id, ftp_pw)
+
+ftp.cwd("./html")
+
+my_file = open(f"coupang/{file_name}", "rb")
+
+ftp.storlines(f"STOR {file_name}", my_file)
+my_file.close
+ftp.close()
